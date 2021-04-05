21584bfd1425556839f21560ca486e8c36c76bfc
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-05 11:45:01 -0600

    updated

--- Unit-Assessment-5/server/index.js
+++ Unit-Assessment-5/server/index.js
@@ -1,21 +1,12 @@
 require('dotenv').config();
 const express = require('express'),
-      app = express(),
       userCtrl = require('./controllers/user'),
-      postCtrl = require('./controllers/posts'),
-      massive = require('massive'),
-      session = require('express-session'),
-      bcrypt = require('bcryptjs')
+      postCtrl = require('./controllers/posts')
 
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+
+const app = express();
 
 app.use(express.json());
-
-app.use(session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}))
 
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
@@ -29,12 +20,4 @@ app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
 
-massive({
-  connectionString: CONNECTION_STRING, 
-  ssl: {rejectUnauthorized: true}
-})
-  .then(db => {
-    app.set('db', db);
-    app.listen(SERVER_PORT, _ => console.log(`DB is up and running on ${SERVER_PORT}`));
-  })
-
+app.listen(4000, _ => console.log(`running on ${4000}`));
