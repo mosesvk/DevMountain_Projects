3ec10ae28d1c5a1d5b82a6bf3ba5068b012a8f20
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-07 14:13:51 -0600

    update Nav, routes, and package.json

--- devmountain-personal-project/src/routes.js
+++ devmountain-personal-project/src/routes.js
@@ -3,15 +3,17 @@ import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home/home'
 import About from './components/About/about'
 import Programs from './components/Programs/programs'
-import Login from './components/Login/login'
+import Contact from './components/Contact/contact'
 import Register from './components/Register/register'
+import Login from './components/Login/login'
 
 export default (
   <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/programs" component={Programs} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/register" component={Register} />
+    <Route exact path="/"   component={Home} />
+    <Route path="/about"    component={About} />
+    <Route path="/programs" component={Programs} />
+    <Route path="/contact"  component={Contact} />
+    <Route path="/register" component={Register} />
+    <Route path="/login"    component={Login} />
   </Switch>
 )
