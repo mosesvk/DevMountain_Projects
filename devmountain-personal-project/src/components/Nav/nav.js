3309577fa1ac4f45f1c58386803bc8dfeb396acd
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-07 13:59:19 -0600

    update Nav structure

---
+++ devmountain-personal-project/src/components/Nav/nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const nav = () => {
+  return (
+    <div className="nav">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <h1>POWERHOUSE</h1>
+        </div>
+        <div className="nav-links">
+          <Link className="links" to="/">HOME</Link>
+          <Link className="links" to="/">ABOUT</Link>
+          <Link className="links" to="/">PROGRAMS</Link>
+          <Link className="links" to="/">REGISTER</Link>
+          <Link className="links" to="/">LOGIN</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default nav
