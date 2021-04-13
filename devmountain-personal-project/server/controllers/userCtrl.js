392f4378ae372fefdacfeee37c927afb79a8ab6a
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-12 19:38:58 -0600

    update backend

--- devmountain-personal-project/server/controllers/userCtrl.js
+++ devmountain-personal-project/server/controllers/userCtrl.js
@@ -3,7 +3,19 @@ const bcrypt = require('bcryptjs');
 module.exports = {
   login: async(req, res) => {
     const { username, password } = req.body;
-
+    const db = req.app.get('db');
+    const result = await db.get_user({username});
+    const existingUser = result[0];
+    if (!existingUser) {
+        return res.status(409).send('Username Does Not Exist');
+    }
+    const authenticated = bcrypt.compareSync(password, existingUser.password)
+    if (!authenticated) {
+        return res.status(401).send('Password is incorrect')
+    }
+    delete existingUser.password
+    req.session.user = existingUser
+    return res.status(200).send(req.session.user);
   },
   register: async (req, res) => {
     const {username, password, first_name, last_name, email} = req.body;
@@ -17,7 +29,8 @@ module.exports = {
     const hash = bcrypt.hashSync(password, salt);
     const registeredUser = await db.register_user({username, first_name, last_name, email, hash})
     const user = registeredUser[0];
-    req.session.user = {userId: user.user_id, username: user.username};
+    delete user.password
+    req.session.user = user
     return res.status(201).send(req.session.user);
 },
   logout: async(req, res) =>  {
