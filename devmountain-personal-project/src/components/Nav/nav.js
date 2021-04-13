46f3465e10f85dc79e12ae6c17c9ec91d269e0e4
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-13 16:07:03 -0600

    update

--- devmountain-personal-project/src/components/Nav/nav.js
+++ devmountain-personal-project/src/components/Nav/nav.js
@@ -18,15 +18,15 @@ const nav = () => {
         </div>
         <div className="nav-info-wrap">
           <div className="nav-social-wrap">
-            <a className="social-links" href="https://www.google.com/maps/dir/40.2861729,-111.749542/athletic+republic+google+maps/@40.3525274,-111.9417521,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x874d81e6f4f56b71:0x14fb336b3b3a2144!2m2!1d-111.8334535!2d40.3858813">
+            <a className="social-links" href="https://www.google.com/maps/dir/40.2861729,-111.749542/athletic+republic+google+maps/@40.3525274,-111.9417521,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x874d81e6f4f56b71:0x14fb336b3b3a2144!2m2!1d-111.8334535!2d40.3858813" target="_blank" rel="noreferrer">
               <FaLocationArrow/>
               <Link className="social-links">Lehi, UT</Link>
             </a>
             <Link className="social-links" to="/login">LOGIN</Link>
-            <a href="https://www.facebook.com/TuniK78" className="social-links social-icons"><FaFacebookSquare/></a>
-            <a href="https://www.instagram.com/t.kanuch78/?hl=en" className="social-links social-icons"><FaInstagram/></a>
-            <a href="https://www.linkedin.com/in/tuni-kanuch-b00673140/" className="social-links social-icons"><FaLinkedin/></a>
-            <a href="https://twitter.com/kanuch78" className="social-links social-icons"><FaTwitterSquare/></a>
+            <a href="https://twitter.com/kanuch78" target="_blank" rel="noreferrer" className="social-links social-icons"><FaTwitterSquare/></a>
+            <a href="https://www.facebook.com/TuniK78" target="_blank" rel="noreferrer" className="social-links social-icons"><FaFacebookSquare/></a>
+            <a href="https://www.instagram.com/t.kanuch78/?hl=en" target="_blank" rel="noreferrer" className="social-links social-icons"><FaInstagram/></a>
+            <a href="https://www.linkedin.com/in/tuni-kanuch-b00673140/" target="_blank" rel="noreferrer" className="social-links social-icons"><FaLinkedin/></a>
           </div>
           <div className="nav-links-wrap">
             <Link className="links" to="/">HOME</Link>
