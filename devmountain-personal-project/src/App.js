9ab5ca9f5f021d944e9752b940e9c47d27b2546f
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-13 18:22:51 -0600

    update navbar

--- devmountain-personal-project/src/App.js
+++ devmountain-personal-project/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {HashRouter} from 'react-router-dom'
 import Nav from './components/Nav/nav'
 import routes from './routes'
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <HashRouter>
       <Nav />
--- devmountain-personal-project/src/components/Nav/nav.js
+++ devmountain-personal-project/src/components/Nav/nav.js
@@ -5,11 +5,14 @@ import {
   FaLinkedin,
   FaInstagram,
   FaLocationArrow,
-  FaTwitterSquare
+  FaTwitterSquare,
+  FaBars
 } from 'react-icons/fa';
 import './nav.scss'
 
-const nav = () => {
+const nav = (props) => {
+  const { toggle } = props;
+
   return (
     <div className="nav">
       <div className="nav-container">
@@ -18,11 +21,11 @@ const nav = () => {
         </div>
         <div className="nav-info-wrap">
           <div className="nav-social-wrap">
-            <a className="social-links" href="https://www.google.com/maps/dir/40.2861729,-111.749542/athletic+republic+google+maps/@40.3525274,-111.9417521,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x874d81e6f4f56b71:0x14fb336b3b3a2144!2m2!1d-111.8334535!2d40.3858813" target="_blank" rel="noreferrer">
+            <a className="social-links phone-size-link" href="https://www.google.com/maps/dir/40.2861729,-111.749542/athletic+republic+google+maps/@40.3525274,-111.9417521,11z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x874d81e6f4f56b71:0x14fb336b3b3a2144!2m2!1d-111.8334535!2d40.3858813" target="_blank" rel="noreferrer">
               <FaLocationArrow/>
               <Link className="social-links">Lehi, UT</Link>
             </a>
-            <Link className="social-links" to="/login">LOGIN</Link>
+            <Link className="social-links phone-size-link" to="/login">LOGIN</Link>
             <a href="https://twitter.com/kanuch78" target="_blank" rel="noreferrer" className="social-links social-icons"><FaTwitterSquare/></a>
             <a href="https://www.facebook.com/TuniK78" target="_blank" rel="noreferrer" className="social-links social-icons"><FaFacebookSquare/></a>
             <a href="https://www.instagram.com/t.kanuch78/?hl=en" target="_blank" rel="noreferrer" className="social-links social-icons"><FaInstagram/></a>
@@ -36,6 +39,9 @@ const nav = () => {
             <Link className="links link-box" id="join-box" to="/register">JOIN</Link>
             {/* <Link className="links" to="/register">REGISTER</Link> */}
             
+          </div>
+          <div className="nav-bars" onClick={toggle}>
+            <FaBars />
           </div>
         </div>
       </div>
