468647f484f5e327519590c8e4bbb6e53199fbe3
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-14 16:51:08 -0600

    update

--- devmountain-personal-project/src/components/Register/register.js
+++ devmountain-personal-project/src/components/Register/register.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './register.scss'
 
 const register = () => {
@@ -13,33 +14,32 @@ const register = () => {
     //     </div>
     //   </div>
     // </div>
-    
-    <div className="container">
-      <div className="box" id="sign-up">
-        <a className="back" href="#back"><span className="back-arrow">Back</span></a>
-        <div className="brand">
-          <div className="logo">
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
+    <div className="register-container">
+      <div className="register-wrap">
+        <div className="box" id="sign-up">
+          <a className="back" href="/home"><span className="back-arrow">Back</span></a>
+          <div className="brand">
+            <div className="logo">
+              <div className="square"></div>
+              <div className="square"></div>
+              <div className="square"></div>
+            </div>
+            <h2 className="title">Sign up now</h2>
           </div>
-          <h2 className="title">Sign up now</h2>
-        </div>
-        <div className="form">
-          <form>
-            <div className="row">
-              <input type="text" name="firstname" placeholder="First name"/>
-              <input type="text" name="lastname" placeholder="Last name"/>
-            </div>
-            <input type="email" name="email" placeholder="Email"/>
-            <input type="password" name="password" placeholder="Password"/>
-            <div className="checkbox">
-              <input type="checkbox" name="terms" id="terms"/>
-              <label for="terms" className="checkbox-label"></label>
-              <label for="terms" className="description">I agree with <a href="#terms">Terms & conditions</a></label>
-            </div>
-            <button type="submit" name="submit">Sign up for free</button>
-          </form>
+          <div className="form">
+            <form>
+              <div className="row">
+                <input type="text" name="firstname" placeholder="First name"/>
+                <input type="text" name="lastname" placeholder="Last name"/>
+              </div>
+              <input type="email" name="email" placeholder="Email"/>
+              <input type="password" name="password" placeholder="Password"/>
+              <div className="reg-sign-up">
+                <p>Already a member?</p><Link>Sign in</Link>
+              </div>
+              <button type="submit" name="submit">Sign up for free</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
