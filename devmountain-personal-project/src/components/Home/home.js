7d116445ce01d7a10c915e0a9af18e7fe7946f30
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-13 21:52:11 -0600

    updating top main section

--- devmountain-personal-project/src/components/Home/home.js
+++ devmountain-personal-project/src/components/Home/home.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
 import './home.scss'
 
 const home = () => {
   return (
     <div>
-      <div className="top-container">
+      <div className="top-container" >
         <div className="top-wrap">
           <h1>GET READY TO</h1>
           <div className="top-text-box">
-            <h1>WORK</h1>
+            <Carousel controls={false} indicators={false} >
+              <Carousel.Item interval={1500}>
+                <h1>WORK</h1>
+              </Carousel.Item>
+              <Carousel.Item interval={1500}>
+                <h1>DOMINATE</h1>
+              </Carousel.Item>
+              <Carousel.Item interval={1500}>
+                <h1>PUSH</h1>
+              </Carousel.Item>
+            </Carousel>
           </div>
         </div>
       </div>
