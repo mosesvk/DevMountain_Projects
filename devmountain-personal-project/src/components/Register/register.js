c5fa119e309d8bc19a58b96b94e6b0a035c52472
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-14 16:05:16 -0600

    updating register page

--- devmountain-personal-project/src/components/Register/register.js
+++ devmountain-personal-project/src/components/Register/register.js
@@ -4,7 +4,14 @@ import './register.scss'
 const register = () => {
   return (
     <div className='register-container'>
-
+      <div className="register-panel">
+        <div className="register-panel-header">
+          <h5>SIGN UP</h5>
+        </div>
+        <div className="register-panel-body">
+          
+        </div>
+      </div>
     </div>
   )
 }
