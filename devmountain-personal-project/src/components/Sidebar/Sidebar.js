8eb70c4fe6857f09ed183c2ada69f9b3b28bbe27
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-13 18:02:23 -0600

    updating sidebar

--- devmountain-personal-project/src/components/Sidebar/Sidebar.js
+++ devmountain-personal-project/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import {FaTimes} from 'react-icons/fa'
+import './Sidebar.scss'
 
 const Sidebar = () => {
   
@@ -10,8 +13,26 @@ const Sidebar = () => {
 
   return (
     <div className='sidebar-container' isOpen={isOpen} toggle={toggle}>
-      <div></div>
-      <div></div>
+      <div className="icon-wrap">
+        <FaTimes />
+      </div>
+      <div className="sidebar-wrap">
+        <div className="sidebar-menu">
+          <Link to="/"
+            onClick={toggle}>HOME</Link>
+          <Link to="/about"
+            onClick={toggle}>ABOUT</Link>
+          <Link to="/contact"
+            onClick={toggle}>CONTACT</Link>
+          <Link to="/programs"
+            onClick={toggle}>PROGRAMS</Link>
+          <Link to="/register"
+            onClick={toggle}>JOIN</Link>
+          <Link to="/login"
+            onClick={toggle}>LOGIN</Link>
+          <hr/>
+        </div>
+      </div>
     </div>
   )
 }
