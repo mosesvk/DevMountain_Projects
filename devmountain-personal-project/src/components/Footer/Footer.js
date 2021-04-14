581c3d6e2651776fe54b740b584efc310bca631c
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-14 13:43:35 -0600

    update

--- devmountain-personal-project/src/components/Footer/Footer.js
+++ devmountain-personal-project/src/components/Footer/Footer.js
@@ -3,10 +3,8 @@ import {
   FaFacebookSquare,
   FaLinkedin,
   FaInstagram,
-  FaTwitterSquare,
-  FaRegStar
+  FaTwitterSquare
 } from 'react-icons/fa';
-import Divider from '@material-ui/core/Divider';
 import {Link} from 'react-router-dom'
 import './Footer.scss'
 
@@ -14,32 +12,18 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="footer-wrap">
-        <div className="footer-top">
-          <Link>JOIN NOW</Link>
-          <div className="footer-top-review">
-            <p>Please leave us a review</p>
-            <FaRegStar/>
-            <FaRegStar/>
-            <FaRegStar/>
-            <FaRegStar/>
-            <FaRegStar/>
-          </div>
+        <div className="info-wrap">
+          <Link className="footer-link">PRIVACY</Link>
+          <Link className="footer-link">TERMS</Link>
+          <Link className="footer-link">CANCEL</Link>
+          <Link className="footer-link">CONTACT</Link>
+          <Link className="footer-link">LOGIN</Link>
         </div>
-        <Divider dark/>
-        <div className="footer-bottom">
-          <div className="info-wrap">
-            <Link>PRIVACY</Link>
-            <Link>TERMS</Link>
-            <Link>CANCEL</Link>
-            <Link>CONTACT</Link>
-            <Link>LOGIN</Link>
-          </div>
-          <div className="social-wrap">
-            <a href="https://twitter.com/kanuch78" target="_blank" rel="noreferrer" className="social-links social-icons"><FaTwitterSquare/></a>
-            <a href="https://www.facebook.com/TuniK78" target="_blank" rel="noreferrer" className="social-links social-icons"><FaFacebookSquare/></a>
-            <a href="https://www.instagram.com/t.kanuch78/?hl=en" target="_blank" rel="noreferrer" className="social-links social-icons"><FaInstagram/></a>
-            <a href="https://www.linkedin.com/in/tuni-kanuch-b00673140/" target="_blank" rel="noreferrer" className="social-links social-icons"><FaLinkedin/></a>
-          </div>
+        <div className="social-wrap">
+          <a href="https://twitter.com/kanuch78" target="_blank" rel="noreferrer" className="social-footer-links"><FaTwitterSquare/></a>
+          <a href="https://www.facebook.com/TuniK78" target="_blank" rel="noreferrer" className="social-footer-links"><FaFacebookSquare/></a>
+          <a href="https://www.instagram.com/t.kanuch78/?hl=en" target="_blank" rel="noreferrer" className="social-footer-links"><FaInstagram/></a>
+          <a href="https://www.linkedin.com/in/tuni-kanuch-b00673140/" target="_blank" rel="noreferrer" className="social-footer-links"><FaLinkedin/></a>
         </div>
       </div>
     </div>
