02e35983f429e61e2eafc63e9d7b4c344505082a
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-14 15:28:15 -0600

    update

--- devmountain-personal-project/src/components/Home/home.js
+++ devmountain-personal-project/src/components/Home/home.js
@@ -10,13 +10,13 @@ const home = () => {
           <h1 className="top-h1">GET READY TO</h1>
           <div className="top-text-box">
             <Carousel controls={false} indicators={false} >
-              <Carousel.Item class="carousel-item" interval={1500}>
+              <Carousel.Item interval={1500}>
                 <h1 className="top-carousel-h1">WORK</h1>
               </Carousel.Item>
-              <Carousel.Item class="carousel-item" interval={1500}>
+              <Carousel.Item interval={1500}>
                 <h1 className="top-carousel-h1">DOMINATE</h1>
               </Carousel.Item>
-              <Carousel.Item class="carousel-item" interval={1500}>
+              <Carousel.Item interval={1500}>
                 <h1 className="top-carousel-h1">PUSH</h1>
               </Carousel.Item>
             </Carousel>
@@ -25,7 +25,15 @@ const home = () => {
       </div>
       <div className="mid-container">
         <div className="mid-wrap">
-
+          <div className="mid-wrap-info">
+            <h1>WE'RE ON A MISSION TO ADVANCE YOUR SKILLS TO PERFORM EFFECTIVELY ON THE FIELD</h1>
+            <h3>We specialize in offensive lineman specific techniques and drills. </h3>
+            <img
+              className="mid-img"
+              src="https://images-na.ssl-images-amazon.com/images/I/61bNYjc2tiL._AC_SX679_.jpg"
+              alt="Second slide"
+            />
+          </div>
         </div>
       </div>
     </div>
