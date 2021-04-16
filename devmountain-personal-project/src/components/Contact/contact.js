a8ea3d5d1e03472455df7d596b008a9a8c8d78cf
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-15 21:47:03 -0600

    update ;

--- devmountain-personal-project/src/components/Contact/contact.js
+++ devmountain-personal-project/src/components/Contact/contact.js
@@ -10,7 +10,8 @@ import "./contact.scss";
 const contact = () => {
   return (
     <section id="contact">
-      <h1 class="section-header">CONTACT</h1>
+      <h1 class="section-header">REACH OUT & LET'S CHAT</h1>
+      <p class="contact-p">Feel free to reach out through Social Media as well</p>
       <div class="contact-wrapper">
           {/* <!---------------- 
           CONTACT PAGE LEFT 
@@ -28,38 +29,35 @@ const contact = () => {
           </div>
 
           <textarea class="form-control" rows="10" placeholder="MESSAGE" name="message" required></textarea>
-          <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND"/>
-            <div class="button">
-              <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
-            </div>
+          <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">SEND</button>
         </div>
           {/* <!---------------- 
           CONTACT PAGE RIGHT 
           ----------------->  */}
-          <div class="direct-contact-container">
-            <ul class="contact-list">
-              <li class="list-item"><i class="fa fa-map-marker fa-2x"><span class="contact-text place">Provo, Utah</span></i></li>
-              <li class="list-item"><i class="fa fa-phone fa-2x"><span class="contact-text phone"><a href="tel:1-212-555-5555" title="Give me a call">(212) 555-2368</a></span></i></li>
-              <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:#" title="Send me an email">kanuch78@gmail.com</a></span></i></li>
-            </ul>
-            <hr/>
-            <ul class="social-media-list">
-              <li><a href="https://www.facebook.com/TuniK78" target="_blank" rel="noreferrer" class="contact-icon">
-                <FaFacebookSquare/></a>
-              </li>
-              <li><a href="https://twitter.com/kanuch78" target="_blank" rel="noreferrer" class="contact-icon">
-              <FaTwitterSquare/></a>
-              </li>
-              <li><a href="https://www.instagram.com/t.kanuch78/?hl=en" target="_blank" rel="noreferrer" class="contact-icon">
-              <FaInstagram/></a>
-              </li>
-              <li><a href="https://www.youtube.com/watch?app=desktop&v=mSa3CtE7DSE" target="_blank" rel="noreferrer" class="contact-icon">
-              <FaYoutube/></a>
-              </li>
-            </ul>
-            <hr/>
-            <div class="copyright">&copy; ALL OF THE RIGHTS RESERVED | Moses Kaumatule</div>
-          </div>
+        <div class="direct-contact-container">
+          <ul class="contact-list">
+            <li class="list-item"><i class="fa fa-map-marker fa-2x"><span class="contact-text place">Provo, Utah</span></i></li>
+            <li class="list-item"><i class="fa fa-phone fa-2x"><span class="contact-text phone"><a href="tel:1-212-555-5555" title="Give me a call">(212) 555-2368</a></span></i></li>
+            <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:#" title="Send me an email">kanuch78@gmail.com</a></span></i></li>
+          </ul>
+          <hr/>
+          <ul class="social-media-list">
+            <li><a href="https://www.facebook.com/TuniK78" target="_blank" rel="noreferrer" class="contact-icon">
+              <FaFacebookSquare/></a>
+            </li>
+            <li><a href="https://twitter.com/kanuch78" target="_blank" rel="noreferrer" class="contact-icon">
+            <FaTwitterSquare/></a>
+            </li>
+            <li><a href="https://www.instagram.com/t.kanuch78/?hl=en" target="_blank" rel="noreferrer" class="contact-icon">
+            <FaInstagram/></a>
+            </li>
+            <li><a href="https://www.youtube.com/watch?app=desktop&v=mSa3CtE7DSE" target="_blank" rel="noreferrer" class="contact-icon">
+            <FaYoutube/></a>
+            </li>
+          </ul>
+          <hr/>
+          <div class="copyright">&copy; ALL OF THE RIGHTS RESERVED | Powerhouse Fitness</div>
+        </div>
       </div>
     </section>  
   
